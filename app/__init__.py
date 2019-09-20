50d2aef2ab46f86250f8e6b463d3f242d3c398a0
Craig Derington <cderington@championsg.com>
2019-09-20 16:24:42 -0400

    Add nmap function - views.py + template updates.

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ from qpylib import qpylib
 # define flask app
 app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///qradar.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://netauto:netauto@0.0.0.0:5432/netauto"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///qradar.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://netauto:netauto@0.0.0.0:5432/netauto"
 app.config["SECRET_KEY"] = os.urandom(64)
 app.config.update()
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, Response, redirect, url_for, flash
 from datetime import datetime, timedelta
+import subprocess
+import ipaddress
 
 
 @app.route("/")
@@ -12,5 +14,62 @@ def index():
     )
 
 
+@app.route("/nmap", methods=["GET", "POST"])
+def nmap():
+    """
+    Endpoint for nmap scan
+    :return template
+    """
+    total_hosts = 0
+    hosts = None
+    ip = None
+
+    if request.method == "POST":
+        ip = request.args.get("ip_network", default="10.0.1.0/24")
+        try:
+            cidr = ipaddress.IPv4Network(unicode(ip, "utf-8"))
+            
+            # set hosts to our network scan response
+            hosts = scan_network(cidr)
+            total_hosts = len(hosts)/2 - 2
+
+        except ipaddress.AddressValueError as err:
+            # log the error
+            msg = "The IP Address {} CIDR range is invalid: {}".format(str(ip), str(err))
+            flash(msg, category="danger")
+            return redirect(url_for("index"))    
+
+    return render_template(
+        "nmap.html",
+        ip=ip,
+        hosts=hosts,
+        total_hosts=int(total_hosts),
+        today=get_date()
+    )
+
 def get_date():
     return datetime.now().strftime("%c")
+
+
+def scan_network(cidr):
+    """
+    Scan the CIDR using subprocess check_output
+    :param CIDR network
+    :return hosts <list>
+    """
+    hosts = None
+    network = str(cidr)
+
+    try:
+        data = subprocess.check_output(
+            ["/usr/local/bin/nmap", "-sP", "-n", network],
+            stderr=subprocess.STDOUT
+        )
+        # set hosts to a list split on new line
+        hosts = data.split("\n")
+    
+    except subprocess.CalledProcessError as err:
+        print(str(err))
+
+
+    return hosts
