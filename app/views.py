a3ecff21228293ca2fae73dc77949fef402bec82
Craig Derington <cderington@championsg.com>
2019-09-25 15:55:15 -0400

    Add qradar health checks and update nmap scan types.

--- app/views.py
+++ app/views.py
@@ -3,11 +3,20 @@ from flask import render_template, request, Response, redirect, url_for, flash
 from datetime import datetime, timedelta
 import subprocess
 import ipaddress
+import requests
+
+BASE_URL = "https://my.api.mockaroo.com/"
+hdrs = {"content-type": "application/json", "X-API-Key": "f6f21d00"}
 
 
 @app.route("/")
 @app.route("/index")
 def index():
+    """ 
+    endpoint for index
+    :param none
+    :return template
+    """
     return render_template(
         "index.html",
         today=get_date()
@@ -17,12 +26,20 @@ def index():
 @app.route("/nmap", methods=["GET", "POST"])
 def nmap():
     """
-    Endpoint for nmap scan
+    endpoint for nmap scan
     :return template
     """
     total_hosts = 0
     hosts = None
     ip = None
+    scan_types = {
+        "Fast Scan": 1, 
+        "Aggressive Scan": 2, 
+        "Multi-Port Scan": 3, 
+        "Service Version Scan": 4,
+        "OS Version Scan": 5, 
+        "Vulnerability Scan": 6
+    }
 
     if request.method == "POST":
         ip = request.form["ip_network"]
@@ -37,18 +54,95 @@ def nmap():
             # log the error
             msg = "The IP Address {} CIDR range is invalid: {}".format(str(ip), str(err))
             flash(msg, category="danger")
-            return redirect(url_for("index"))    
+            return redirect(url_for("nmap"))    
 
     return render_template(
         "nmap.html",
         ip=ip,
         hosts=hosts,
         total_hosts=int(total_hosts),
+        scan_types=scan_types,
         today=get_date()
     )
 
+
+@app.route("/qradar", methods=["GET", "POST"])
+def qradar():
+    """
+    endpoint for qradar health check data
+    :param none
+    :return template
+    """
+    security_endpoint = "/health_data_-_security_data_count.json"
+    top_offenses_endpoint = "/health_data_-_top_offenses.json"
+    top_rules_endpoint = "/health_data_-_top_rules.json"
+    security_data, top_offenses, top_rules = None, None, None
+
+    try:
+        security_data = call_qradar_api(security_endpoint)
+        
+        offenses = call_qradar_api(top_offenses_endpoint)
+        if offenses:
+            top_offenses = {}
+            for offense in offenses:
+                top_offenses["offense_name"] = offense["offense_name"]
+                top_offenses["offense_count"] = offense["count"]
+                top_offenses.update()
+
+        rules = call_qradar_api(top_rules_endpoint)
+        if rules:
+            top_rules = {}
+            for rule in rules:
+                top_rules["rule_name"] = rule["rule_name"]
+                top_rules["rule_count"] = rule["count"]
+                top_rules.update() 
+
+    except Exception as err:
+        msg = "Unable to contact the QRadar API server for health data: {}".format(str(err))
+        flash(msg, category="danger")
+        return redirect(url_for("index"))
+
+    return render_template(
+        "qradar.html",
+        today=get_date(),
+        security_data=security_data,
+        top_offenses=top_offenses,
+        top_rules=top_rules
+    )
+
+
 def get_date():
     return datetime.now().strftime("%c")
+
+
+def call_qradar_api(endpoint):
+    """ 
+    Base REST API function
+    :param endpoint
+    :return json obj
+    """
+    resp = None
+
+    if not isinstance(endpoint, str):
+        endpoint = str(endpoint)
+    
+    try:
+        r = requests.request(
+            "GET",
+            BASE_URL + endpoint,
+            headers=hdrs
+        )
+
+        if r.status_code == 200:
+            resp = r.json()
+        else:
+            resp = "HTTP returned status code: {}".format(str(r.status_code))
+    
+    except requests.HTTPError as http_err:
+        resp = "HTTP Error: {}".format(str(http_err))
+    
+    return resp
+
 
 
 def scan_network(cidr):
@@ -70,6 +164,5 @@ def scan_network(cidr):
     
     except subprocess.CalledProcessError as err:
         print(str(err))
-
 
     return hosts
